7e57c41f94d3c92109ca197c7e93ebe8df0cda04
Ryan Miller <ryansmiller@gmail.com>
2016-02-29 19:57:27 -0500

    Add FontAwesome to functions.php

--- src/functions.php
+++ src/functions.php
@@ -124,6 +124,8 @@ function rock_scripts() {
 
 	wp_enqueue_style( 'bootstrap-css', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '3.3.6', 'all' );
 
+	wp_enqueue_style( 'font-awesome', get_template_directory_uri() . '/css/font-awesome.min.css', array(), '4.5.0', 'all' );
+
 	wp_enqueue_style( 'snapchat', get_template_directory_uri() . '/css/fa-snapchat.css', array(), '0.1.2', 'all' );
 
 	wp_enqueue_script( 'jquery', get_template_directory_uri() . '/js/jquery.min.js', array(), '2.2.0', false );
